589dee25fc18dc907ad0298b175da9e953deb7e8
Daniel Laurence <daniellaurence269@gmail.com>
2018-02-15 12:24:28 -0500

    Add comments to map js.

--- js/map.js
+++ js/map.js
@@ -1,10 +1,13 @@
 /* ------- Map ------- */
 function initMap() {
+    // Instantiate Map with Rome as Center
     var rome = {lat: 41.887330, lng: 12.485204};
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
         center: rome
     });
+
+    // List of Markers
     var Pantheon = {lat: 41.898834, lng: 12.477452};
     var marker = new google.maps.Marker({
         position: Pantheon,
