a015fb88b1784b13d8fd16db5e794c1be754f820
Daniel Laurence <daniellaurence269@gmail.com>
2018-02-27 16:18:30 -0500

    Close infowindows when opening a new one.

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-/* - Side Panel - */
+/* --- Side Panel --- */
 $(document).ready(function(){
     $(".collapse-expand").click(function(){ 
         $("#all-minus").toggle("slide");
@@ -17,28 +17,43 @@ function initMap() {
     });
 
     // List of Markers
-    results.forEach(function(result) {
-        var marker = new google.maps.Marker({
+    var markers = results.map(function(result) {
+        return new google.maps.Marker({
             position: {lat: result.lat, lng: result.lng},
             map: map,
             animation: google.maps.Animation.DROP,
             title: result.name
         });
+    });
         
-        // Info Window
-        var infowindow = new google.maps.InfoWindow({
-          content: '<p class="infowindow-title">' + result.name + '</p>'
+    // List of Info Windows
+    var infoWindows = markers.map(function(marker) {
+        return new google.maps.InfoWindow({
+            content: '<p class="infowindow-title">' + marker.title + '</p>'
         });
-        // Click to open Info Window
+    });
+
+    // Click to close all Info Windows
+    function closeInfoWindows() {
+        infoWindows.forEach(function(infoWindow) {
+            infoWindow.close()
+        });
+    }
+
+    // Click to open Info Window
+    markers.forEach(function(marker, index) {
         marker.addListener('click', function() {
-            infowindow.open(map, marker);
+            closeInfoWindows();
+            infoWindows[index].open(map, marker);
         });
-        // Selected marker bounces
-        marker.addListener('click', function() {
-            marker.setAnimation(google.maps.Animation.BOUNCE);
-            setTimeout(function(){ marker.setAnimation(null); }, 700);
-        });
-    })  
+    });
+    
+    // // Selected marker bounces
+    // marker.addListener('click', function() {
+    //     marker.setAnimation(google.maps.Animation.BOUNCE);
+    //     setTimeout(function(){ marker.setAnimation(null); }, 700);
+    // });
+      
 }
 
 
