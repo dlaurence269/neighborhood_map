cde20248c4d6fd9b9db670b13ecf0847f3e14822
Daniel Laurence <daniellaurence269@gmail.com>
2018-05-29 15:13:41 -0400

    Replace Twitter API with Yelp API test business
    All of the Twitter API files were removed. Yelp API working
    with one test business.

--- server.js
+++ server.js
@@ -26,44 +26,14 @@ app.get('/', function (request, response) {
 })
 
 
-/* ------- Variable Declarations ------- */
-
-var twitterConsumerKey = "VpuOze8qyzJNSC9QBTBN0sAdO";
-var twitterConsumerSecret = "BW9yR0oqLvpD1gatSTWTlNRacWmiOINnMufw9S7QpKwgOBvM4s";
-var accessToken = "";
-
-
 /* ------- Helper Methods ------- */
-
-function encodeTwitterCredentials(key, secret) {
-    return btoa(key + ":" + secret);
-}
-
-function getTwitterAccessToken() {
-    request({
-        uri: "https://api.twitter.com/oauth2/token",
-        method: "POST",
-        headers: {
-                "Authorization":"Basic " + encodeTwitterCredentials(twitterConsumerKey, twitterConsumerSecret),
-                "Content-Type":"application/x-www-form-urlencoded;charset=UTF-8"
-            },
-        form: {
-            grant_type: "client_credentials",
-        }
-    }, function(errorMessage, response, data) {
-            console.log("ERROR:", errorMessage);
-            var json = JSON.parse(data);
-            accessToken = json["access_token"];
-            authenticateToken();
-    });
-}
 
 function authenticateToken() {
     request({
-        uri: "https://api.twitter.com/1.1/search/tweets.json?q=%23pantheon&geocode=41.887330,12.485204,5km&include_entities=true&count=1",
+        uri: "https://api.yelp.com/v3/businesses/pantheon-basilica-di-santa-maria-ad-martyres-roma",
         method: "GET",
         headers: {
-                "Authorization":"Bearer " + "AAAAAAAAAAAAAAAAAAAAAAbb5QAAAAAAZCQzE%2FCOpxjxLgS0sp7l7l2wYKY%3DvVMkUZXLnJ1ssT4ueL1OyY7CV7OjkZkemuhS6spJuusEAuRexX",
+                "Authorization":"Bearer " + "S4XcB3erCr3mv3ZsCdnTCpjNXY0vTFo-3E6KCA7fbtQ1XtLPVwJQ7pKxH0EEFuAUCDEqwN67anPUjtxAZwvKnn1CZt9xd9wub7R6CMBXj044MF788sYhdVj12ZoNW3Yx",
             },
         timeout: 10000,
         followRedirect: true,
@@ -74,4 +44,4 @@ function authenticateToken() {
     });
 }
 
-getTwitterAccessToken();
+authenticateToken();
