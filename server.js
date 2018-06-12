2a73622c10d64c734b0e410ad27bda7789f809cb
Daniel Laurence <daniellaurence269@gmail.com>
2018-06-12 14:55:26 +0300

    feat: Replace test data with actual yelp data by chaining promises
    Used a mixture of ES7 await/async and the Promises provided by the
    'request-promise' module to group the promises together. I think
    the biggest gotcha was that I had to wrap the .map call for each
    request in Promise.all to convert the array of Promises into one
    Promise that the calling function could call .then on.

--- server.js
+++ server.js
@@ -1,77 +1,76 @@
 /* ------- Backend for Application ------- */
 
 // Node equivalent of importing library, in this case load express, path, jquery, btoa etc.
-// dotenv helps get enviromental variables
+// dotenv helps get enviromental constiables
 require('dotenv').config()
-var express = require('express');
-var path = require('path');
-var request = require('request');
-var results = require('./static/js/compiled/data');
+const express = require('express');
+const path = require('path');
+const request = require('request-promise');
+const results = require('./static/js/compiled/data');
 
+const app = express();
+const port = 8000;
 
-var app = express();
-var port = 8000;
-var allData = [];
-var test = [
-    { "alias": "accademia-di-francia-villa-medici-roma", "rating": 4.5, "review_count": 10, yelpURL: "#" },
-    { "alias": "pantheon-basilica-di-santa-maria-ad-martyres-roma", "rating": 4.5, "review_count": 357, yelpURL: "#" },
-    { "alias": "basilica-di-san-pietro-roma-4", "rating": 5, "review_count": 350, yelpURL: "#" },
-    { "alias": "cappella-sistina-roma", "rating": 4.5, "review_count": 113, yelpURL: "#" },
-    { "alias": "colosseo-roma", "rating": 4.5, "review_count": 801, yelpURL: "#" }
-];
-
+/* ------- Configuration ------- */
 app.listen(port, function () {
     console.log(`Neighborhood app listening on port ${port}!`);
-})
+});
 
 // Enable access to style files within static directory
-app.use(express.static('static'))
+app.use(express.static('static'));
 
-
-app.get('/', function (request, response) {
+// Default route to index.html file
+app.get('/', function (request, response, next) {
     response.sendFile(path.join(__dirname + '/index.html'));
-})
+});
 
+/* ------- REST Endpoints ------- */
 app.get('/yelpReviewData', function (request, response) {
-    console.log("GET request to /yelpReviewData made. Returning yelp data:", test);
-    response.send(test);
+    console.log("GET request to /yelpReviewData made");
+
+    fetchAllYelpDetails(results).then(all => {
+        console.log("Fetching successful.");
+        response.send(all);
+    }).catch(function(error) {
+        console.error("Fetching failed for endpoint '/yelpReviewData'.", error);
+        next(error);
+    });
 });
 
 
 /* ------- Helper Methods ------- */
 
-function authenticateToken(location) {
+function fetchYelpDetail(location) {
     // FIXME what to do with missing alias
-    var yelpBusinessID = [location.alias, location.queryString].join("?");
+    const yelpBusinessID = [location.alias, location.queryString].join("?");
     return request({
         uri: "https://api.yelp.com/v3/businesses/" + yelpBusinessID,
         method: "GET",
         headers: {
-                "Authorization":"Bearer " + process.env.YELP_ACCESS_TOKEN,
-            },
+            "Authorization":"Bearer " + process.env.YELP_ACCESS_TOKEN,
+        },
         timeout: 10000,
         followRedirect: true,
         maxRedirects: 10
-    }, function(errorMessage, response, data) {
-        if(errorMessage) {
-            console.error(errorMessage, "Yelp data not fetched.");
-        } else {
-            json = JSON.parse(data);
-            // rename alias to yelpBusinessID
-            // rename url to yelpURL
-            console.log(json.alias, json.rating, json.review_count, json.url);
-            return json;
-        }
-            
     });
 }
 
-function authenticateEachToken(results) {
-    var allData = results.map(function(result) {
-        var yelpData = authenticateToken(result);
-        return yelpData;
-    });
-    return allData;
+async function fetchAllYelpDetails(results) {
+    async function fetchOne(result) {
+        return await fetchYelpDetail(result).then(function(data) {
+            const json = JSON.parse(data);
+            return {
+                alias: json.alias,
+                rating: json.rating,
+                review_count: json.review_count,
+                yelpURL: json.url
+            };
+        }).catch(function(error) {
+            console.error("Yelp data not fetched.", error.message);
+            // We return an empty object so that an empty object shows up in the
+            // results to the front end
+            return {};
+        });
+    }
+    return await Promise.all(results.map(fetchOne));
 }
-
-var allData = authenticateEachToken(results);
